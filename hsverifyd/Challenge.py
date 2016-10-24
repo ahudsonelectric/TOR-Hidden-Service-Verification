8403c027ce6b135bd5004e8332abebdb47462b72
arrase <arrase@gmail.com>
2016-10-25 01:15:03 +0200

    flask go away

--- hsverifyd/Challenge.py
+++ hsverifyd/Challenge.py
@@ -1,5 +1,26 @@
 import gnupg
+import json
+from threading import Thread
 
-class Challenge:
-    def __init__(self):
-        pass
+
+class ChallengeThread(Thread):
+    _conn = None
+    _config = None
+
+    def __init__(self, conn, config):
+        Thread.__init__(self)
+        self._config = config
+        self._conn = conn
+
+    def run(self):
+        # Get data from client
+        data = json.loads(self._conn.recv(2048))
+        # Check valid json
+        if not data.has_key("challenge"):
+            self._conn.send('{"status":"false"}')
+            self._conn.close()
+            return False
+        # TODO: Resolve challenge
+        # Respond to user
+        self._conn.send('{"status":"true"}')
+        self._conn.close()
--- hsverifyd/Server.py
+++ hsverifyd/Server.py
@@ -1,15 +1,34 @@
-from flask import Flask
+import socket
+from sys import exit
+
+from Challenge import ChallengeThread
 
 
-class Server:
-    _app = None
+class Server():
+    _log = None
+    _config = None
+    _tcpServer = None
 
-    def __init__(self):
-        self._app = Flask(self)
+    threads = []
 
-    @_app.route('/')
-    def index(self):
-        return "<h1>Hi Grandma!</h1>"
+    def __init__(self, log, config):
+        self._log = log
+        self._config = config
 
     def run(self):
-        self._app.run()
+        try:
+            self._tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._tcpServer.bind((self._config.ip, self._config.port))
+        except socket.error:
+            self._log.error("There was an error when trying to bind the server")
+            exit(2)
+        finally:
+            self._log.info("Waiting for connections ...")
+
+        while True:
+            self._tcpServer.listen(4)
+            (conn, (ip, port)) = self._tcpServer.accept()
+            newthread = ChallengeThread(conn, self._config)
+            newthread.start()
+            self.threads.append(newthread)
