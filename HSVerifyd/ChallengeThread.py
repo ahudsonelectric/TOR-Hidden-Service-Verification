0b992a8b8cbf355c2ea7fc911a2ba94add19c892
arrase <arrase@gmail.com>
2016-10-31 00:59:23 +0100

    set headers

--- HSVerifyd/ChallengeThread.py
+++ HSVerifyd/ChallengeThread.py
@@ -30,7 +30,10 @@ class ChallengeThread(BaseHTTPRequestHandler):
             # If request is Unknown
             response['HSVerifyd'] = False
 
+        # Send response
+        json_response = json.dumps(response)
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
+        self.send_header('Content-length', len(json_response))
         self.end_headers()
-        self.wfile.write(json.dumps(response))
+        self.wfile.write(json_response)
